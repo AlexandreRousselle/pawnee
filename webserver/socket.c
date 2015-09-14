eb6e58231aa4d05bb13c03eb73f538917db8376a
Descamps Mickael <descamps.mickael@git.fr>
2015-09-14 17:09:48 +0200

    Ajout mail descamps

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,6 +7,7 @@ int creer_serveur(int port)
 {
 	int socket_serveur;
 	int socket_client;
+	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
 
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
@@ -19,25 +20,47 @@ int creer_serveur(int port)
 	if(socket_serveur == -1)
 	{
 		perror("socket_serveur");
+		return -1;
 	}
 
 	if(bind(socket_serveur,(struct sockaddr *)&saddr, sizeof(saddr)) == -1)
 	{
 		perror("bind socket_serveur");
+		return -1;
 	}
 
 	if(socket_client == -1)
 	{
 		perror("accept");
+		return -1;
 	}
 
-	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
 	write(socket_client, message_bienvenue, strlen(message_bienvenue);
 
 	if(listen(socket_serveur, 10) == -1)
 	{
 		perror("liste socket_serveur");
+		return -1;
 	}
 
 }
 
+/* Socket () 
+Puis on fait le bind()
+listen () 
+Rien de bloquant dans ces trois la. 
+Sauf si port déjà occupé. 
+
+accept() = > Bloquant, on ne sort pas le temps que personne a demandé une connexion. 
+Algorythme général d'un serveur 
+Socket()
+bind()
+listen()
+
+while(accept()){
+	fork();
+}
+
+Algo géné d'un client : 
+Socket()
+Accept()
