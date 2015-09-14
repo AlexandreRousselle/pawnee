69c001a72aece14369e69ae0b882e1d984f73805
Alexandre Rousselle <roal6262@gmail.com>
2015-09-14 18:04:24 +0200

    Ajout while(1) socket.c

--- webserver/socket.c
+++ webserver/socket.c
@@ -1,47 +1,59 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include "socket.h"
+#include <stdio.h>
+#include <sys/types.h>   
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
-int creer_serveur(int port)
-{
-	int socket_serveur;
+
+int creer_serveur(int port){
+	int socket_serveur;	
 	int socket_client;
-	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
-
+	
 	struct sockaddr_in saddr;
-	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(port);
-	saddr.sin_addr.s_addr = INADDR_ANY;
+	saddr.sin_family = AF_INET ; /* Socket ipv4 */
+	saddr.sin_port = htons(port); /* Port d ’ écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 
-	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	char data[1024];
+
+	socket_serveur = socket(AF_INET,SOCK_STREAM, 0);
+
+	if (socket_serveur == -1){
+		perror ("socket_serveur");
+		return -1;
+	}
+
+	if (bind(socket_serveur,(struct sockaddr *)& saddr, sizeof(saddr)) == -1){
+		perror("bind socker_serveur");
+		return -1;
+	}
+
+	if (listen(socket_serveur, 10) == -1){
+		perror("listen socket_serveur");
+		return -1;
+	}
+
 	socket_client = accept(socket_serveur, NULL, NULL);
 
-	if(socket_serveur == -1)
-	{
-		perror("socket_serveur");
-		return -1;
-	}
-
-	if(bind(socket_serveur,(struct sockaddr *)&saddr, sizeof(saddr)) == -1)
-	{
-		perror("bind socket_serveur");
-		return -1;
-	}
-
-	if(socket_client == -1)
-	{
+	if (socket_client == -1){
 		perror("accept");
 		return -1;
 	}
 
-	write(socket_client, message_bienvenue, strlen(message_bienvenue);
+	const char * msg = "Bonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\n";
+	write(socket_client, msg, strlen(msg));
 
-	if(listen(socket_serveur, 10) == -1)
-	{
-		perror("liste socket_serveur");
-		return -1;
-	}
+	while(1){
+		if(read(socket_client, data, sizeof(data)) == -1){
+			perror("listen socket_serveur");
+			return -1;
+		}
+		write(socket_client, data, strlen(data));
+	}	
+
+	return socket_client;
 
 }
 
@@ -63,4 +75,4 @@ while(accept()){
 
 Algo géné d'un client : 
 Socket()
-Accept()
+Accept() */
