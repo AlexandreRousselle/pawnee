18115c309adf900b884ca8f7e13576f26e476534
Alexandre Rousselle <roal6262@gmail.com>
2015-09-22 16:30:58 +0200

    serveur tcp normal presque fini

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include "socket.h"
+#include <unistd.h>
 
 
 int main(void)
 {
-	creer_serveur(8080);
+	fflush(stdout);
+	int socket_serveur = creer_serveur(8080);
+	int socket_client;
+
+	char * data = malloc(42);
+
+	while(1){
+		socket_client = accept(socket_serveur, NULL, NULL);
+
+		if (socket_client == -1){
+			perror("accept");
+
+		}
+		
+		sleep(1);
+
+		const char * msg = "Bonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\n";
+		write(socket_client, msg, strlen(msg));
+	
+
+		while(1){
+			read(socket_client, data, 42);
+			write(socket_client, data, 42);
+		}
+
+	}
 	return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -8,54 +8,28 @@
 
 
 int creer_serveur(int port){
-	int socket_serveur;	
-	int socket_client;
+	int socket_serveur;
 	
+	socket_serveur = socket(AF_INET,SOCK_STREAM, 0);
+
+	if (socket_serveur == -1){
+		perror ("socket_serveur");
+	}
+
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET ; /* Socket ipv4 */
 	saddr.sin_port = htons(port); /* Port d ’ écoute */
 	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 
-	char data[1024];
-
-	socket_serveur = socket(AF_INET,SOCK_STREAM, 0);
-
-	if (socket_serveur == -1){
-		perror ("socket_serveur");
-		return -1;
-	}
-
 	if (bind(socket_serveur,(struct sockaddr *)& saddr, sizeof(saddr)) == -1){
 		perror("bind socker_serveur");
-		return -1;
 	}
 
 	if (listen(socket_serveur, 10) == -1){
 		perror("listen socket_serveur");
-		return -1;
 	}
 
-	socket_client = accept(socket_serveur, NULL, NULL);
-
-	if (socket_client == -1){
-		perror("accept");
-		return -1;
-	}
-	
-	sleep(1);
-
-	const char * msg = "Bonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\nBonjour,\nBienvenue sur mon serveur\n";
-	write(socket_client, msg, strlen(msg));
-
-	while(1){
-		if (read(socket_client, data, sizeof(data)) == -1){
-			perror("listen socket_serveur");
-			return -1;
-		}
-		write(socket_client, data, strlen(data));
-	}
-
-	return socket_client;
+	return socket_serveur;
 
 }
 
